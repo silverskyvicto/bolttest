f974472efaaa46781dc9cc73c0ee963804c4fb26
Yuuki Ebihara <yuuki.ebihara813@gmail.com>
2019-11-24 15:32:32 +0900

    First Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const { App } = require('@slack/bolt');
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
